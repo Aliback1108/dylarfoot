ac5eb651ec54ec5131fab626ca97d3d573786817
KimBgey <andrekimgbaguidi01@gmail.com>
2026-04-15 18:27:31 +0100

    Refont frontend et ajout de page about and contact

--- app.py
+++ app.py
@@ -237,6 +237,15 @@ def index():
                            away_team=away_team, home_logo=home_logo, away_logo=away_logo, error=error,
                            home_stats=home_stats, away_stats=away_stats, is_vip=is_vip, league_teams=league_teams)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    print(app.url_map)
+    app.run(host="0.0.0.0", port=port, debug=True)
